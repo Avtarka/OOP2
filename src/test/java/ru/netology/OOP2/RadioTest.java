c7385fc2d4434dfafba03fcb8f5f4724ad490778
Pavel Kryuchkov <mr.p@bk.ru>
2023-06-24 19:01:23 +0400

    Add tests

--- src/test/java/ru/netology/OOP2/RadioTest.java
+++ src/test/java/ru/netology/OOP2/RadioTest.java
@@ -1,3 +1,183 @@
 package ru.netology.OOP2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class RadioTest {
+    //Тесты функции переключения каналов:
+    @Test
+    public void setCurrentStationInZero() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentStation(0);
+        int expected = 0;
+        int actual = rad.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void setCurrentStationOverMinimum() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentStation(-1);
+        int expected = 0;
+        int actual = rad.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void setCurrentStationOverMaximum() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentStation(10);
+        int expected = 0;
+        int actual = rad.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void setCurrentStationNormal() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentStation(5);
+        int expected = 5;
+        int actual = rad.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void previousStation1() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentStation(9);
+        rad.previousStation();
+        int expected = 8;
+        int actual = rad.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void previousStation2() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentStation(0);
+        rad.previousStation();
+        int expected = 9;
+        int actual = rad.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void nextStation1() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentStation(8);
+        rad.nextStation();
+        int expected = 9;
+        int actual = rad.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void nextStation2() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentStation(9);
+        rad.nextStation();
+        int expected = 0;
+        int actual = rad.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
+    //Тесты установки и переключения функции громкости:
+    @Test
+    public void setVolumeMax() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentVolume(100);
+        int expected = 100;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void setVolumeMin() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentVolume(0);
+        int expected = 0;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void setVolumeBorderMin() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentVolume(1);
+        int expected = 1;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void setVolumeBorderMax() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentVolume(99);
+        int expected = 99;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void setVolumeOverMinimum() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentVolume(-1);
+        int expected = 0;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void setVolumeOverMax() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentVolume(101);
+        int expected = 0;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void increaseVolumeMin() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentVolume(0);
+        rad.increaseVolume();
+        int expected = 1;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void increaseVolumeMax() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentVolume(99);
+        rad.increaseVolume();
+        int expected = 100;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void increaseVolumeOverMax() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentVolume(100);
+        rad.increaseVolume();
+        int expected = 100;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void decreaseVolumeMaxBorder() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentVolume(100);
+        rad.decreaseVolume();
+        int expected = 99;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void decreaseVolumeMinBorder() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentVolume(1);
+        rad.decreaseVolume();
+        int expected = 0;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void decreaseVolumeOverMinBorder() {
+        ru.netology.OOP2.Radio rad = new Radio();
+        rad.setCurrentVolume(0);
+        rad.decreaseVolume();
+        int expected = 0;
+        int actual = rad.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
 }
